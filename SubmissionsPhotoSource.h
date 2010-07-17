fc27173da16c52a18f2160c13205067bbc2c7e30
Joe Cannatti <joecannatti@gmail.com>
2010-07-17 10:14:47 -0400

    facebook three20

---
+++ SubmissionsPhotoSource.h
@@ -0,0 +1,16 @@
+//
+//  SubmissionsPhotoSource.h
+//  SPACES
+//
+//  Created by Joe Cannatti Jr on 7/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Three20/Three20.h"
+
+@interface SubmissionsPhotoSource : TTThumbsViewController {
+
+}
+
+@end
