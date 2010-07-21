233913f18b87786b72a19860b8ad967c0dd872ab
Randy Beiter <rbb@sideways.com>
2010-07-20 22:11:59 -0400

    added wrapper shell script

---
+++ tweetapp/cronContestFetch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#over the web method
+#/usr/local/bin/setlock -n /tmp/cronlock.3782158186.81354.$$ sh -c $'/usr/bin/curl -s http\072//bloggedupspaces.org/tweetapp/cronContestFetch.php 1\076\0462 \046\076/dev/null'
+
+#local method
+export PATH=${PATH}:/usr/local/bin
+PHP_SCRIPT=${HOME}/bloggedupspaces.org/tweetapp/cronContestFetch.php
+cd `dirname ${PHP_SCRIPT}`
+/usr/local/bin/setlock -n /tmp/cronlock.3782158186.81354.$$ php ${PHP_SCRIPT} 2>&1 > /dev/null
